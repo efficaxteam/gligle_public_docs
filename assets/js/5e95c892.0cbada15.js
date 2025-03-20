bf087cf2b43d635cc7a66e1598d1a8be1c1187e3
GivenFLY <GivenFLY@users.noreply.github.com>
2025-03-20 18:36:31 +0000

    deploy: 8a9354c15ca02ea2281cc678b2b370b639ba1ceb

---
+++ assets/js/5e95c892.0cbada15.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgligle_docs=self.webpackChunkgligle_docs||[]).push([[647],{8122:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var c=r(4164),l=r(1769),u=r(204),a=r(2831),d=r(1410),n=r(4848);function t(e){return(0,n.jsx)(l.e3,{className:(0,c.A)(u.G.wrapper.docsPages),children:(0,n.jsx)(d.A,{children:(0,a.v)(e.route.routes)})})}}}]);
---
+++ assets/js/a7456010.a7cc0ca9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkgligle_docs=self.webpackChunkgligle_docs||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
